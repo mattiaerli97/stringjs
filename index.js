0c7c67bb80c99b41be33e62fbfcd35dcdfef4598
Mattia Erli <erli.mattia@gmail.com>
2021-03-12 19:57:38 +0100

    capitalizeByIndex

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ module.exports.capitalizeLast = (str) => {
     const strLength = str.length
     return str.slice(0, strLength - 1) + str.charAt(strLength - 1).toUpperCase()
 }
+
+module.exports.capitalizeByIndex = (str, idx) => {
+    const strLength = str.length
+    switch (idx) {
+        case 0:
+            return this.capitalizeFirst(str)
+        case strLength - 1:
+            return this.capitalizeLast(str)
+        default:
+            const charToUppercase = str.charAt(idx).toUpperCase()
+            return str.slice(0, idx) + charToUppercase + str.slice(idx + 1, strLength)
+    }
+}
