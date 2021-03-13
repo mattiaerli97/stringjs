12f48421324f2316a4dacd5e471cacf87eeedf6e
Mattia Erli <erli.mattia@gmail.com>
2021-03-13 13:01:57 +0100

    rollup uglify and devDependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,10 @@
+import { uglify } from 'rollup-plugin-uglify'
+
 export default {
     input: 'src/index.js',
     output: {
        file: 'bundle/bundle.js',
        format: 'cjs'
-    }
+    },
+    plugins: [ uglify() ]
   };
