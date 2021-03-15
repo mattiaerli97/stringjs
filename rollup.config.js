975aca01ab403c50cd427b4484d9879d1a5d9413
Mattia Erli <mattia.erli@serioplast.com>
2021-03-15 08:46:27 +0100

    Removed Uglify

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,7 @@
-import { uglify } from 'rollup-plugin-uglify';
-
 export default {
   input: 'src/index.js',
   output: {
     file: 'bundle/bundle.js',
     format: 'cjs',
-  },
-  plugins: [uglify()],
+  }
 };
