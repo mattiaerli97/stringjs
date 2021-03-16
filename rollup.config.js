77d61a932bb2d55506a7aa828f09834f0944f051
Mattia Erli <erli.mattia@gmail.com>
2021-03-16 20:00:50 +0100

    Improved bundle size

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,10 @@
+import { terser } from "rollup-plugin-terser";
+
 export default {
   input: 'src/index.js',
   output: {
     file: 'bundle/bundle.js',
     format: 'cjs',
-  }
+  },
+  plugins: [terser()]
 };
